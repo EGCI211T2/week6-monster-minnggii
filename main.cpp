637dbc6bfd80aec0359cf198651ce7727dfd3369
minnggii <54227292+minnggii@users.noreply.github.com>
2026-02-11 04:31:12 +0000

    updated

--- main.cpp
+++ main.cpp
@@ -5,7 +5,21 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 
-    monster A;
+    monster A("Siri", 10, 1);
+    monster B("Daisuke");
+    monster *p; //No constructor is called
+    //monster x[5] = {{"James"}, {"Michael", 20}, {"Sarah"}, {"Amy"}, {"Sam"}};
+    monster c;
+    p = &c;
+    p -> display();
+    p = &A;
+    p -> display();
+    p = new monster("Sarina", 20);
+    p -> display();
+    delete p;
+
+    p = new monster[3];
+    delete []p;
   /*
  
  monster *p,x;
--- monster.h
+++ monster.h
@@ -5,9 +5,31 @@ class monster{
 private:
 	string name;
 	int hp,potion;
+
 public:
 	void Attack(monster &);
-          void heal();
-	monster(string, int,int);
+        void heal();
+	monster(string = "Anonymous", int = 1, int = 1);
+	//Alt + 126 for "~"
+	~monster();
+	void display()
+	{
+		cout<<"Name: " <<name <<endl;
+		cout<<"HP: " <<hp <<endl;
+		cout<<"Potions: " <<potion <<endl;
+	}
 };
+
+monster::~monster()
+{
+	cout<<name <<" is gone"<<endl;
+}
+
+monster::monster(string n, int h, int p)
+{
+	name = n;
+	hp = h;
+	potion = p;
+	cout<<"Monster "<<name <<" is here"<<endl;
+}
 #endif
